75c3bbe818ed548dedf283c714cc2166db0fbf4e
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-07-06 16:31:48 -0300

    Agregar componente footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script>
 import TheHeader from "./components/TheHeader.vue"
+import TheFooter from "./components/TheFooter.vue"
 
 export default {
     components: {
         TheHeader,
+        TheFooter,
     }
 }
 </script>
 
 <template>
 <TheHeader />
-<router-view></router-view>
+<main>
+    <router-view></router-view>
+</main>
+<TheFooter />
 </template>
 
 <style>
